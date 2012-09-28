75de04de499111138288dae6cfa6a61e0c6a8c45
crazyquark <Cristian@Cristian-PC.lan>
2012-09-29 00:57:49 +0300

    minor

--- src/sort.py
+++ src/sort.py
@@ -6,3 +6,9 @@ Created on Sep 29, 2012
 
 from sorter import *
 
+if __name__ == '__main__':
+    pass
+
+
+
+
---
+++ src/sorter/engine.py
@@ -0,0 +1,10 @@
+'''
+Created on Sep 29, 2012
+
+@author: Cristian Sandu
+'''
+
+class SortingEngine:
+    def __init__(self):
+        pass
+

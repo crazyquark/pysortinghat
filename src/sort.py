f7514cdc0093a49ee5b8eb1e8125f287c938193d
crazyquark <Cristian@Cristian-PC.lan>
2012-09-29 16:30:57 +0300

    some code, finally

--- src/sort.py
+++ src/sort.py
@@ -4,10 +4,23 @@ Created on Sep 29, 2012
 @author: Cristian Sandu
 '''
 
-from sorter import *
+import sys
+import sorter
+
+
+def main():
+    print "The Sorting Hat is loading params"
+    # if we have no params, the default values will be used
+    if len(sys.argv) > 4:
+        sorter.config.Config.ClutterDir = sys.argv[1]
+        sorter.config.Config.MoviesDir = sys.argv[2]
+        sorter.config.Config.TvDir = sys.argv[3]
+    else:
+        print '** No params specified, using default values **'
+        print 'Usage: sort.py clutter_dir movies_dir tv_dir'
 
 if __name__ == '__main__':
-    pass
+    main()
 
 
 
--- src/sorter/config.py
+++ src/sorter/config.py
@@ -5,3 +5,10 @@ Created on Sep 29, 2012
 '''
 
 
+class Config:
+    def __init__(self):
+        # Where the videos will be neatly aranged
+        self.MoviesDir = 'G:\\Movies'
+        self.TvDir = 'G:\\TV'
+        # Where the mess is
+        self.ClutterDir = 'G:\\Torrents'

cdd769738afded98b3e872aa98112a589fde4535
csandu <csandu@cristians-mini.buc.gameloft.org>
2013-10-08 19:16:28 +0300

    Works for OS X; compiles libunrar and installs it

--- deps/makedeps.sh
+++ deps/makedeps.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-wget ftp://ftp.rarlabs.com/rar/unrarsrc-5.0.12.tar.gz
-wget https://raw.github.com/mirror/comical/master/unrar/makefile.macosx
+echo "Installing Python deps, requires sudo pass please type it..."
+sudo easy_install termcolor
+sudo easy_install rarfile
 
+echo "Get UnRAR src..."
+wget ftp://ftp.rarlabs.com/rar/unrarsrc-5.0.12.tar.gz
+
+echo "Compiling libunrar..."
+tar xzvf unrarsrc-*.tar.gz
+cd unrar
+
+make lib
+mv libunrar.so ../libunrar.dylib
+
+echo "Move it in the right place..."
+sudo cp ../libunrar.dylib /usr/lib/
 

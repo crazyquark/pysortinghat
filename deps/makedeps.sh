17b0e54406436515d16d6171d4b3162ca2da1f5e
Cristian Sandu <cristian.sandu@gmail.com>
2014-05-06 18:05:06 +0300

    Modify makdeps.sh to work on Linux as well. Tested on RPi.

--- deps/makedeps.sh
+++ deps/makedeps.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+UNAME = `shell uname` 
 
-echo "Installing Python deps, requires sudo pass please type it..."
-sudo easy_install termcolor
-sudo easy_install rarfile
+echo "Installing Python deps, requires sudo pass please type it..." sudo easy_install termcolor sudo easy_install rarfile
 
 echo "Get UnRAR src..."
 wget ftp://ftp.rarlabs.com/rar/unrarsrc-5.0.12.tar.gz
@@ -12,7 +11,13 @@ tar xzvf unrarsrc-*.tar.gz
 cd unrar
 
 make lib
+if [[ $UNAME == *Darwin* ]]; then
 mv libunrar.so ../libunrar.dylib
+fi
 
 echo "Move it in the right place..."
+if [[ $UNAME == *Darwin* ]]; then
 sudo cp ../libunrar.dylib /usr/lib/
+else
+sudo cp libunrar.so /usr/lib/
+fi

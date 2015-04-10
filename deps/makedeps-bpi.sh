2cba5e0945dd99873bcf3ec7f83e43dbf9a5c1ca
root <root@bananapi.(none)>
2015-04-10 21:37:56 +0300

    Add setup script for Bananian(the environment is slightly different from Raspbian/Debian)

---
+++ deps/makedeps-bpi.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+UNAME = `shell uname` 
+
+echo "Installing Python deps, requires sudo pass please type it..." 
+apt-get install python3-pip
+pip3 install termcolor 
+pip3 install rarfile
+pip3 install guessit
+
+echo "Get UnRAR src..."
+wget ftp://ftp.rarlabs.com/rar/unrarsrc-5.0.12.tar.gz
+
+echo "Compiling libunrar..."
+tar xzvf unrarsrc-*.tar.gz
+cd unrar
+
+make lib
+if [[ $UNAME == *Darwin* ]]; then
+mv libunrar.so ../libunrar.dylib
+fi
+
+echo "Move it in the right place..."
+if [[ $UNAME == *Darwin* ]]; then
+cp ../libunrar.dylib /usr/lib/
+else
+cp libunrar.so /usr/lib/
+fi

25c73e82f4d4df5d4b5b8feb799713bb107d0c3b
root <root@bananapi.(none)>
2015-04-23 06:39:24 +0300

    Fixed sort.sh

--- sort.sh
+++ sort.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python3 /myhome/dev/pysortinghat/src/sort.py /storage/_completed/ /storage/\!Media/Movies/ /storage/\!Media/TV/
-curl --data-binary '{ "jsonrpc": "2.0", "method": "VideoLibrary.Scan", "id": "mybash"}' -H 'content-type: application/json;' http://192.168.1.63/jsonrpc
+python3 /myhome/dev/pysortinghat/src/sort.py /storage/_completed/ /storage/_Media/Movies/ /storage/_Media/TV/
+curl --data-binary '{ "jsonrpc": "2.0", "method": "VideoLibrary.Scan", "id": "mybash"}' -H 'content-type: application/json;' http://192.168.1.213:8080/jsonrpc
 echo ''
